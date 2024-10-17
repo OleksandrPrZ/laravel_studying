2688caa75f0e1bc8076c0dfbc9a34f6508754a0a
Oleksandr Perebykovskyi <mobile322@gmail.com>
2024-10-17 17:33:18 +0200

    test deploy

--- app/Http/Controllers/Admin/Color/ColorController.php
+++ app/Http/Controllers/Admin/Color/ColorController.php
@@ -74,7 +74,7 @@ class ColorController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function delete1(string $id)
+    public function delete(string $id)
     {
         Color::query()->findOrFail($id)->delete();
 
